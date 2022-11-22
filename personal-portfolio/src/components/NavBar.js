168d300419cee51f6deddba7ac42fc3323505dcd
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-11-22 15:48:54 +0100

    NavBar

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -1,5 +1,28 @@
+import { NavBar, Container, Navbar, Nav } from "react-bootstrap";
+
 export const NavBar = () => {
   return (
-
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={''} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#"></a>
+            </div>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
